33b401ce339018ca031a7788f6c7d2927f57a002
devlearnerlp <89476182+devlearnerlp@users.noreply.github.com>
2025-04-29 16:40:45 +0530

    Create CopyStatus.java

---
+++ src/CopyStatus.java
@@ -0,0 +1,9 @@
+public class CopyStatus {
+    private String collection;
+    private long totalDocs;
+    private long copiedDocs;
+    private String status; // "IN_PROGRESS", "SUCCESS", "FAILED"
+    private String errorMessage;
+
+    // Constructors, Getters, Setters
+}
