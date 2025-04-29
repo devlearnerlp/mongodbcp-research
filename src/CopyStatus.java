502b614a8734614b55939c9159453f0ab1d154c6
devlearnerlp <89476182+devlearnerlp@users.noreply.github.com>
2025-04-29 16:42:36 +0530

    Update CopyStatus.java

--- src/CopyStatus.java
+++ src/CopyStatus.java
@@ -2,8 +2,8 @@ public class CopyStatus {
     private String collection;
     private long totalDocs;
     private long copiedDocs;
-    private String status; // "IN_PROGRESS", "SUCCESS", "FAILED"
+    private String status; // "IN_PROGRESS", "PAUSED", "SUCCESS", "FAILED"
     private String errorMessage;
-
-    // Constructors, Getters, Setters
+    private boolean pauseRequested = false;
 }
+
